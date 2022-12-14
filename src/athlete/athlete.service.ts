f1192a1fd327cd67ecdcce35b9e317b6c2bbf966
stieber93 <83284165+stieber93@users.noreply.github.com>
2022-12-14 15:04:07 +0100

    commented athlete service

--- src/athlete/athlete.service.ts
+++ src/athlete/athlete.service.ts
@@ -5,23 +5,42 @@ import { Athlete } from './entities/athlete.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-@Injectable()
+@Injectable() // @Injectable() tells Nest that this is a class, that can have dependencies
 export class AthleteService {
   constructor(
+    // Injects the Athlete Repository into the Athlete Service
     @InjectRepository(Athlete)
     private athleteRepository: Repository<Athlete>,
   ) {}
   
+  /**
+   * CRUD Method "CREATE" to create a new athlete
+   * @author Steffen Reuter
+   * @param createAthleteDto 
+   * @returns new created athlete
+   */
   create(createAthleteDto: CreateAthleteDto): Promise<Athlete> {
-    const newAthlete = this.athleteRepository.create(createAthleteDto);
+    const newAthlete = this.athleteRepository.create(createAthleteDto); // creates new athlete
     return this.athleteRepository.save(newAthlete);
   }
 
+  /**
+   * CRUD Method "findAll (READ)" to read all athletes
+   * @author Steffen Reuter
+   * @returns all athletes 
+   */
   findAll(): Promise<Athlete[]> {
     return this.athleteRepository.find();
   }
 
+  /**
+   * CRUD Method "findOne (READ)" to read a specific athlete
+   * @author Steffen Reuter
+   * @param id 
+   * @returns a specific athlete or error if not found
+   */
   async findOne(id: number): Promise<Athlete> {
+    // searching a specific athelte --> if found, return this athlete / if not found, throw exception error
     const found = await this.athleteRepository.findOneBy({ id });
     if (!found) {
       throw new NotFoundException(`Sportler mit der ID: "${id}" konnte nicht gefunden werden!`);
@@ -30,13 +49,26 @@ export class AthleteService {
     }
   }
 
+  /**
+   * CRUD Method "UPDATE" to update an existing athlete
+   * @author Steffen Reuter
+   * @param id 
+   * @param updateAthleteDto 
+   * @returns the updated athlete
+   */
   async update(id: number, updateAthleteDto: UpdateAthleteDto): Promise<Athlete> {
+    // searching a specific athlete by id --> changing one or more properties --> return updated athlete
     const athlete = await this.findOne(id);
     athlete.firstname = updateAthleteDto.firstname;
     athlete.lastname = updateAthleteDto.lastname;
     return this.athleteRepository.save(athlete);
   }
 
+  /**
+   * CRUD Method "DELETE" to delete an existing athlete
+   * @author Steffen Reuter
+   * @param id 
+   */
   async remove(id: number): Promise<void> {
     await this.athleteRepository.delete(id);
   }
